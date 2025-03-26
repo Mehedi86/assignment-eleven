200fe60d28072a20be6543a7d7e0ec4acc4b91af
Mehedi86 <abdullahalmehedi@gmail.com>
2025-03-26 11:29:42 +0600

    dependencies setup

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2 className='text-6xl'>This is main layout</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
