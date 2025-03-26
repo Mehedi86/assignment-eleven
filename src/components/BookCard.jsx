3531a30079fb175bc5eea844df9e43a22ebf6a94
Mehedi86 <abdullahalmehedi@gmail.com>
2025-03-26 19:13:42 +0600

    added: all books page

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const BookCard = ({book}) => {
-    const {author, category, image, name, quantity, rating} = book;
+const BookCard = ({ book }) => {
+    const { author, category, image, name, quantity, rating } = book;
     return (
-        <div className='flex justify-center'>
-            <h2>{author}</h2>
+        <div className='flex justify-center shadow-sm'>
+            <div className="card bg-base-100 w-full flex flex-col justify-between">
+                <figure>
+                    <img
+                        src='/history.jpg'
+                        alt="Shoes" />
+                </figure>
+                <div className="p-4">
+                    <h2 className="text-xl font-bold text-[#059669]">{name}</h2>
+                    <p className='text-gray-400'>{author}</p>
+                    <p><span className='font-bold'>Category:</span> {category}</p>
+                </div>
+                <button className='btn btn-sm mt-2 bg-[#525252] text-white mb-4 w-20 mx-4'>Details</button>
+            </div>
         </div>
     );
 };
--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -9,7 +9,7 @@ const AllBooks = () => {
     return (
         <div className='bg-[#F3E8FF]'>
             <h2 className='text-5xl text-center font-bold p-12'>All Books</h2>
-            <div className='container mx-auto grid grid-cols-4'>
+            <div className='container mx-auto grid grid-cols-6 gap-4'>
                 {books.map(book => <BookCard key={book.id} book={book} />)}
             </div>
         </div>
