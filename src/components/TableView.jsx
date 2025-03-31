067736d7aa9a1e15a44d775d8eddbf17b5fbf2d2
Mehedi86 <abdullahalmehedi@gmail.com>
2025-03-31 23:00:58 +0600

    added: some fix in table

--- src/components/TableView.jsx
+++ src/components/TableView.jsx
@@ -13,10 +13,10 @@ const TableView = ({ books }) => {
                             <th>#</th>
                             <th>Book cover</th>
                             <th>Name</th>
+                            <th>Author</th>
+                            <th>Rating</th>
                             <th>Category</th>
                             <th>Subcategory</th>
-                            <th>Borrowed Date</th>
-                            <th>Return Date</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -28,10 +28,10 @@ const TableView = ({ books }) => {
                                     <img src={book.image || "N/A"} alt="Company Logo" className="w-12 h-12" />
                                 </td>
                                 <td>{book.name}</td>
+                                <td>{book.author}</td>
+                                <td>{book.rating}</td>
                                 <td>{book.category}</td>
                                 <td>{book.subcategory}</td>
-                                <td>{book.borrowed_date}</td>
-                                <td>{book.return_date}</td>
                                 <td className='space-x-2'>
                                     <Link to={`/books/details/${book._id}`} className='btn btn-sm mt-2 bg-[#525252] text-white mb-4 w-20 ml-4'>Details</Link>
                                     <Link to={`/updateBook/${book._id}`} className='btn btn-sm mt-2 bg-[#525252] text-white mb-4 w-20'>Update</Link>
